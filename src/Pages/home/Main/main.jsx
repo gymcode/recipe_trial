607aba1f4789534ae27e9e09e37ee05a565dbedd
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-07-15 17:01:16 +0000

    added css

--- src/Pages/home/Main/main.jsx
+++ src/Pages/home/Main/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from '../styles.css'
 
 const MainComponent = ()=>{
     return (
-        <div>
-            hello home
+        <div className={"container"}>
+            {/* main section  */}
+            <div className={"mainSection"}>
+                <div style={{flex: 2}}>dsdas</div>
+                <div style={{flex: 5}}>das</div>
+                <div style={{flex: 3}}>das</div>
+            </div>
         </div>
     )
 }
