e05f4778065ae1bbb0cffe3eb418d4a52d77c3d5
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-07-16 22:54:46 +0000

    woohooo

--- src/Pages/home/Main/main.jsx
+++ src/Pages/home/Main/main.jsx
@@ -6,9 +6,9 @@ const MainComponent = ()=>{
         <div className={"container"}>
             {/* main section  */}
             <div className={"mainSection"}>
-                <div style={{flex: 2}}>dsdas</div>
-                <div style={{flex: 5}}>das</div>
-                <div style={{flex: 3}}>das</div>
+                <div className={"subSection1"}>dsdas</div>
+                <div className={"subSection2"}>das</div>
+                <div className={"subSection3"}>das</div>
             </div>
         </div>
     )
