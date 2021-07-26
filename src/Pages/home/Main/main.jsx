b8fad5d0693debedcdc2d1f0e4e8140b05cfe38c
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-07-26 12:04:36 +0000

    added navigation to the main folder

--- src/Pages/home/Main/main.jsx
+++ src/Pages/home/Main/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import styles from '../styles.css'
 
+// components 
+import Navigation from '../../../components/nav'
+
 const MainComponent = ()=>{
     return (
         <div className={"container"}>
             {/* main section  */}
             <div className={"mainSection"}>
                 <div className={"subSection1"}>
-                    <div></div>
+                     {/* navigation section  */}
+                    <Navigation/>
                 </div>
                 <div className={"subSection2"}>second</div>
                 <div className={"subSection3"}>third</div>
---
+++ src/components/nav/navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = ()=>{
+    return (
+        <div>
+            we move
+        </div>
+    )
+}
+
+export  default Navigation
