9dc1fd106c4b9d149b607603cc8d3e1bc52d2fc8
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-07-26 12:10:23 +0000

    added some links to the navigation

--- src/components/nav/navigation.js
+++ src/components/nav/navigation.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
 
 const Navigation = ()=>{
     return (
         <div>
-            we move
+            <div>
+                <Link>Overview</Link>
+            </div>
+            <div>
+                <Link>Recipe</Link>
+            </div>
+            <div>
+                <Link>Favorite</Link>
+            </div>
+            <div>
+                <Link>Community</Link>
+            </div>
+            <div>
+                <Link>Setting</Link>
+            </div>
         </div>
     )
 }
